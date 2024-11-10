03d512c890dc33fc96967acd676980789d6ed489
Vision295 <theophilewachtel@gmail.com>
2024-11-10 22:33:10 +0100

    testv2

--- game/right.bash
+++ game/right.bash
@@ -11,10 +11,14 @@ while [ $x_b -lt $(($size_x-1)) ] && [ "${matrix[$y_b,$(($x_b+1))]}" != "X" ]; d
 
         matrix[$y_b,$(($x_b+1))]="B"
         matrix[$y_b,$x_b]="0"
-        moves["$temp,$0"]=$x_b
         ((x_b++))
+
+        moves["$temp,0"]=$x_b 
+        moves["$temp,1"]=$y_b
+        ((temp++))
+
         change_all_pos $x_b $y_b $x_f $y_f
 
     fi
 done
-write_grid_to_file
+write_grid_to_file_public
